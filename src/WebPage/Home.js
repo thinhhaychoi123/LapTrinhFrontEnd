903a83033de89903b6cb3888f3bd5d2da2e5fa1d
NguyenNhatTruong63 <21130176@st.hcmuaf.edu.vn>
2024-07-07 13:48:29 +0700

    chỉnh sửa giao diện detial product

--- src/WebPage/Home.js
+++ src/WebPage/Home.js
@@ -25,9 +25,9 @@ const Home = () => {
                 {tours.map(tour => (
                     <div className="col" key={tour.id}>
                         <div className="card h-100">
-                            <Link to={`/product/${tour.id}`} className="tour-item">
+                            <Link to={`/product/${tour.id}`} className="link-offset-2 link-underline link-underline-opacity-0">
                                 <img src={tour.image} alt={tour.name} className="card-img-top" />
-                                <div className="card-body">
+                                <div className="card-body" style={{height:'296px'}}>
                                     <h5 className="card-title">{tour.name}</h5>
                                     <p className="card-text"><i className="bi bi-calendar"></i> {tour.date}</p>
                                     <p className="card-text"><i className="bi bi-clock"></i> {tour.start_day}</p>
