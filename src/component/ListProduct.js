04c49b0b10d542909c33ddbed960463b21586c02
NguyenNhatTruong63 <21130176@st.hcmuaf.edu.vn>
2024-07-13 15:20:16 +0700

    cập nhập lại code

--- src/component/ListProduct.js
+++ src/component/ListProduct.js
@@ -4,22 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Search from "./Search";
 import { Link } from "react-router-dom";
-import "../css/style.css"
-<<<<<<< HEAD
+import "../css/style.css";
 import Header from "./Header";
-=======
->>>>>>> aa8f3fd81bc3c55f08fc7d6d711ec4bfd61c26ff
 
 const ListProduct = () => {
     const [tours, setTours] = useState([]);
     const [locationEnd, setLocationEnd] = useState('');
+    // chức năng sắp xếp
     const [sortBy, setSortBy] = useState(null);
+    // hiển thị danh sách lọc
     const [filters, setFilters] = useState({
         location: '',
         start: '',
         startDate: ''
     });
     const [searchTerm, setSearchTerm] = useState('');
+
+    // chức năng phân trang
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 10;
 
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
@@ -93,84 +96,86 @@ const ListProduct = () => {
         });
     };
 
-    const filteredTours = getFilteredTours();
-    const sortedTours = sortTours(filteredTours);
-
     const handleSearch = (e) => {
         e.preventDefault();
         // Redirect to ListProduct with search params
         navigate(`/list?location=${filters.location}&start=${filters.start}&startDate=${filters.startDate}&search=${searchTerm}`);
     };
 
+    const filteredTours = getFilteredTours();
+    const sortedTours = sortTours(filteredTours);
+
+    const indexOfLastTour = currentPage * itemsPerPage;
+    const indexOfFirstTour = indexOfLastTour - itemsPerPage;
+    const currentTours = sortedTours.slice(indexOfFirstTour, indexOfLastTour);
+
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
     return (
         <div>
-<<<<<<< HEAD
             <Header />
-=======
->>>>>>> aa8f3fd81bc3c55f08fc7d6d711ec4bfd61c26ff
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearch={handleSearch} filters={filters} />
             <div className="container my-4">
-            <h2 className="mb-4">Tour du lịch Úc từ Hồ Chí Minh</h2>
-            <p>Khám phá Úc với iVIVU: Sydney năng động, Melbourne sành điệu, Uluru huyền bí. Hành trình khám phá văn hóa và thiên nhiên kỳ vĩ. Đặt tour ngay!</p>
-            <div className="row mb-3">
-                <div className="col-md-3 border">
-                    <ul className="list-unstyled">
-                        <li><strong className="fs-5">Tour HOT Nước Ngoài</strong></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Trung Quốc')}>Trung Quốc</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Thái Lan')}>Thái Lan</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Singapore')}>Singapore</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Nhật Bản')}>Nhật Bản</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Châu Âu')}>Châu Âu</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Hàn Quốc')}>Hàn Quốc</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Bali')}>Bali</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Campuchi')}>Campuchi</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Canada')}>Canada</a></li>
-                    </ul>
+                <h2 className="mb-4">Tour du lịch trong nước và ngoài nước</h2>
+                <p>Hãy để chúng tôi dẫn dắt bạn đến những địa điểm tuyệt vời và khám phá những câu chuyện thú vị đằng sau mỗi địa danh trên hành trình của bạn!</p>
+                <div className="row mb-3">
+                    <div className="col-md-3 border">
+                        <ul className="list-unstyled">
+                            <li><strong className="fs-5">Tour HOT Nước Ngoài</strong></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Trung Quốc')}>Trung Quốc</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Thái Lan')}>Thái Lan</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Singapore')}>Singapore</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Nhật Bản')}>Nhật Bản</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Châu Âu')}>Châu Âu</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Hàn Quốc')}>Hàn Quốc</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Bali')}>Bali</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Campuchi')}>Campuchi</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Canada')}>Canada</a></li>
+                        </ul>
 
-                    <ul className="list-unstyled">
-                        <li><strong className="fs-5">Tour HOT Trong Nước</strong></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Hạ Long')}>Hạ Long</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Nha Trang')}>Nha Trang</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Đà Nẵng')}>Đà Nẵng</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Sapa')}>Sapa</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Quy Nhơn')}>Quy Nhơn</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Phú Yên')}>Phú Yên</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Hà Nội')}>Hà Nội</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Buôn Mê Thuật')}>Buôn Mê Thuật</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Phú Quốc')}>Phú Quốc</a></li>
-                        <li><a href="#" onClick={() => handleFilterLocationEnd('Miền Tây')}>Miền Tây</a></li>
-                    </ul>
-                </div>
-                <div className="col-md-9">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                    <div>
-                            <p className="text-muted mb-0">Sắp xếp theo:</p>
-                        </div>
-                        <div>
-                            <button className="btn btn-outline-primary me-2" onClick={() => setSortBy('recommend')}>iVIVU Đề xuất</button>
-                            <button className="btn btn-outline-secondary me-2" onClick={() => setSortBy('duration')}>Thời gian</button>
-                            <button className="btn btn-outline-info me-2" onClick={() => setSortBy('start_date')}>Ngày khởi hành</button>
-                            <button className="btn btn-outline-dark me-2" onClick={() => setSortBy('price')}>Giá</button>
-                        </div>
-                        
+                        <ul className="list-unstyled">
+                            <li><strong className="fs-5">Tour HOT Trong Nước</strong></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Hạ Long')}>Hạ Long</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Nha Trang')}>Nha Trang</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Đà Nẵng')}>Đà Nẵng</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Sapa')}>Sapa</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Quy Nhơn')}>Quy Nhơn</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Phú Yên')}>Phú Yên</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Hà Nội')}>Hà Nội</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Buôn Mê Thuật')}>Buôn Mê Thuật</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Phú Quốc')}>Phú Quốc</a></li>
+                            <li><a href="#" onClick={() => handleFilterLocationEnd('Miền Tây')}>Miền Tây</a></li>
+                        </ul>
                     </div>
+                    <div className="col-md-9">
+                        <div className="d-flex justify-content-between align-items-center mb-3">
+                            <div>
+                                <p className="text-muted mb-0">Sắp xếp theo:</p>
+                            </div>
+                            <div>
+                                <button className="btn btn-outline-primary me-2" onClick={() => setSortBy('recommend')}>iVIVU Đề xuất</button>
+                                <button className="btn btn-outline-secondary me-2" onClick={() => setSortBy('duration')}>Thời gian</button>
+                                <button className="btn btn-outline-info me-2" onClick={() => setSortBy('start_date')}>Ngày khởi hành</button>
+                                <button className="btn btn-outline-dark me-2" onClick={() => setSortBy('price')}>Giá</button>
+                            </div>
+                        </div>
 
-                    {sortedTours.map(tour => (
-                        <div key={tour.id} className="card mb-3 hsshadow" style={{ maxWidth: '800px' }}>
-                            <div className="row g-0">
-                                <div className="col-md-4 d-flex">
-                                    <img src={tour.image} className="img-fluid rounded-start" alt={tour.name} />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{tour.name}</h5>
-                                        <p className="card-text">{tour.date} | {tour.start_day}</p>
-                                        <div className="d-flex justify-content-between">
-                                            <p className="card-text text-success">{tour.evaluate}</p>
-                                            <p className="card-text text-warning fs-5">{tour.price_Adult} VNĐ</p>
-                                        </div>
-                                        <div>
-                                        {cart.find(item => item.id === tour.id) ? (
+                        {currentTours.map(tour => (
+                            <div key={tour.id} className="card mb-3 hsshadow" style={{ maxWidth: '800px' }}>
+                                <div className="row g-0">
+                                    <div className="col-md-4 d-flex">
+                                        <img src={tour.image} className="img-fluid rounded-start" alt={tour.name} />
+                                    </div>
+                                    <div className="col-md-8">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{tour.name}</h5>
+                                            <p className="card-text">{tour.date} | {tour.start_day}</p>
+                                            <div className="d-flex justify-content-between">
+                                                <p className="card-text text-success">{tour.evaluate}</p>
+                                                <p className="card-text text-warning fs-5">{tour.price_Adult} VNĐ</p>
+                                            </div>
+                                            <div>
+                                                {cart.find(item => item.id === tour.id) ? (
                                                     <button
                                                         type="button"
                                                         className="btn btn-danger"
@@ -187,19 +192,29 @@ const ListProduct = () => {
                                                         Thêm vào giỏ hàng
                                                     </button>
                                                 )}
+                                            </div>
+                                            <Link to={`/product/${tour.id}`} className="btn btn-primary">Chi tiết</Link>
                                         </div>
-                                        <Link to={`/product/${tour.id}`} className="btn btn-primary">Chi tiết</Link>
-
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+
+                        <nav>
+                            <ul className="pagination">
+                                {[...Array(Math.ceil(sortedTours.length / itemsPerPage)).keys()].map(number => (
+                                    <li key={number + 1} className="page-item">
+                                        <a onClick={() => paginate(number + 1)} href="#" className="page-link">
+                                            {number + 1}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-        
     );
 };
 
